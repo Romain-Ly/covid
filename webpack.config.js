75919fa956eb6bf9382bc6932346cd77b1842366
Romain Ly <romain.ly1@gmail.com>
2020-05-04 15:11:39 +0200

    webpack: add dist output path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 
     entry: './src/index.tsx',
     output: {
-        path: path.resolve(__dirname, './public'),
+        path: path.resolve(__dirname, './dist'),
         filename: './dist/main.js'
     },
 
